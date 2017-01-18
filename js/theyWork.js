c2c2ad0e713218e4bf1250cf6a7c455d290b7e82
David Trussler <mail@davidtrussler.net>
2017-01-18 22:43:59 +0000

    Comment: Updated to use actual data

--- js/theyWork.js
+++ js/theyWork.js
@@ -10,19 +10,7 @@ var TheyWork = function() {
 
 TheyWork.prototype.init = function() {
   console.log('init!');
-  // console.log('parties: ', this.parties);
 
-  // this.partyData = this._getData();
-  // this._setUpPartyOption();
-
-  // console.log('data: ', this.data);
-
-  /*
-  .then(function() {
-    console.log('promise!');
-  });
-  */
-  // drawData();
   this._getData();
 }
 
@@ -36,13 +24,11 @@ TheyWork.prototype._getData = function() {
   var url = 'http://127.0.0.1/DLT_DataVis_They-work/src/getMPs_response.json';
 
   $.ajax({
-    // dataType: 'jsonp',
     url: url,
     success: function(response) {
       console.log('success!');
 
-      self._parseData();
-      // self._drawData(response);
+      self._parseData(response);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log('error!');
@@ -55,51 +41,37 @@ TheyWork.prototype._getData = function() {
 
 TheyWork.prototype._parseData = function(response) {
   var self = this;
+  var parties = [];
 
-  console.log('_parseData!');
-
-  // simplified with some data to begin
-  this.parties = [
-    {'name': 'Labour', 'count': 34},
-    {'name': 'Tory scum', 'count': 23}
-  ];
-
-  /* create an array of parties
   response.forEach(function(mp) {
-    console.log('party: ', mp.party);
-
-    self.parties.forEach(function(party) {
-      if (party.name == -1) {
-        self.parties.push({
-          'name': mp.party,
-          'count': 0
-        });
-      }
-    });
-  });
-  */
-
-  /* create an array of party counts
-  response.forEach(function(mp) {
-    console.log('party: ', mp.party);
-
-    if (self.parties.indexOf(mp.party) == -1) {
+    if (self.parties.length == 0) {
       self.parties.push({
         'name': mp.party,
-        'data': mp
+        'count': 1
+      });
+
+      parties.push(mp.party);
+    } else if (parties.indexOf(mp.party) == -1) {
+      self.parties.push({
+        'name': mp.party,
+        'count': 1
+      });
+
+      parties.push(mp.party);
+    } else {
+      self.parties.forEach(function(party) {
+        if (party.name == mp.party) {
+          party.count++;
+        }
       });
     }
   });
-  */
 
-  console.log('parties: ', this.parties);
   this._drawData();
 }
 
 TheyWork.prototype._drawData = function() {
-  console.log('_drawData!');
-
-  var dataset = this.parties; // [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+  var dataset = this.parties;
   var w = 500;
   var h = 100;
   var barPadding = 1;
@@ -118,11 +90,11 @@ TheyWork.prototype._drawData = function() {
       return i * (w / dataset.length);
     })
     .attr('y', function(d) {
-      return h - d.count; // h - d;
+      return h - d.count;
     })
     .attr('width', w / dataset.length - barPadding)
     .attr('height', function(d) {
-      return d.count; // d;
+      return d.count;
     })
     .attr('fill', function(d) {
       return "rgb(0, 0, " + d * 5 + ")";
@@ -134,13 +106,13 @@ TheyWork.prototype._drawData = function() {
     .enter()
     .append('text')
     .text(function(d) {
-      return d.name; // d;
+      return d.name;
     })
     .attr('x', function(d, i) {
       return (i * (w / dataset.length)) + ((w / dataset.length - barPadding) / 2);
     })
     .attr('y', function(d) {
-      return d.count; // h - d - 5;
+      return d.count;
     })
     .attr('text-anchor', 'middle')
     .attr('font-family', 'sans-serif')
